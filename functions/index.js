f222c53c6cd5350ba71442508ac34305eb686cad
selami538 <erdaltayfun37@gmail.com>
2025-06-24 16:59:48 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -600,7 +600,7 @@ document.getElementById('searchInput').addEventListener('keyup', function() {
 
 <!-- JS -->
 <script>
-    fetch('https://api.matchkey.sbs/load/matches.json')
+    fetch('https://matchkey.sbs/load/matches.json')
     .then(response => response.text())
     .then(data => {
       document.getElementById('matches-content').innerHTML = data;

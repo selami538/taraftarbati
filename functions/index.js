9bb00300c9abc01f0d52dd6d2a693a4c94e439c0
selami538 <erdaltayfun37@gmail.com>
2025-07-16 22:02:07 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -400,8 +400,8 @@ ${
 ${reklam1 
   ? `<div style="margin: 10px; text-align: center;">
       ${hrefreklam1 
-        ? `<a href="${hrefreklam1}" target="_blank"><img class="ads-img" src="${reklam1}" width="100%"/></a>` 
-        : `<img class="ads-img" src="${reklam1}" width="100%" height="30px"/>`}
+        ? `<a href="${hrefreklam1}" target="_blank"><img class="ads-img" src="${reklam1}" width="100%" height="30px"/></a>` 
+        : `<img class="ads-img" src="${reklam1}" width="100%"/>`}
     </div>` 
   : ''
 }

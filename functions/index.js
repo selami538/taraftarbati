3703ef631a9f75bd4f045148799cd8481f1a2ad7
selami538 <erdaltayfun37@gmail.com>
2025-07-25 23:06:12 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,7 @@ export async function onRequest(context) {
     return String(parseInt(match) + 1);
   });
 
-  const apiUrl = "https://panel.matchkey.sbs/taraftarium/api/verirepo.php";
+  const apiUrl = "https://panelt24.matchkey.sbs/api/verirepo.php";
 
   let title = "";
   let description = "";
